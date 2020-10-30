31201b70cb645930022d82d7fa6f642b2f4a1f78
driesdemaeseneer <syntaxnoob@pop-os.localdomain>
2020-10-30 10:22:50 +0100

    Largest item in vector function created.

--- SelectionSort/SelectionSort.cpp
+++ SelectionSort/SelectionSort.cpp
@@ -1,1 +1,17 @@
 // This file will be edited soon.
+#include <vector>
+#include <iostream>
+
+int Largest(std::vector<int> vector){
+  int a {0};
+  for (int i:vector){
+    if (a<i){
+      a =i;
+    }
+  }
+  return a;
+}
+
+int main(){
+  return 0;
+}
